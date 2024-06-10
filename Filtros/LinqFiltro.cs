aaf7e239ad41bab92610a416396f062fc2337e71
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-06-10 14:27:19 -0300

    Adicionar arquivos de projeto.

---
+++ Filtros/LinqFiltro.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace ScreenSoundAPI.Filtros;
+
+internal class LinqFiltro
+{
+    public static void FiltrarTodosOsGeneros(List<Musica> musicas)
+    {
+        var generos = musicas.Select(g => g.Genero).Distinct().ToList();
+        foreach (var genero in generos)
+        {
+            Console.WriteLine(genero);
+        }
+    }
+
+    public static void ExibirArtistasOrdenados(List<Musica> musicas)
+    {
+        var artistas = musicas.OrderBy(a => a.Artista).Select(a => a.Artista).Distinct().ToList();
+        foreach (var artista in artistas)
+        {
+            Console.WriteLine(artista);
+        }
+    }
+
+    public static void FiltrarArtistasPorGenero(List<Musica> musicas, string genero)
+    {
+        var artistas = musicas.Where(m => m.Genero!.Contains(genero)).Select(a => a.Artista).Distinct().ToList();
+        foreach(var artista in artistas)
+        {
+            Console.WriteLine(artista);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+using ScreenSoundAPI.Filtros;
+using (HttpClient client = new())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //LinqFiltro.FiltrarTodosOsGeneros(musicas);
+        //LinqFiltro.ExibirArtistasOrdenados(musicas);
+        //LinqFiltro.FiltrarArtistasPorGenero(musicas, "pop");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ops, algo deu errado! {ex.Message}");
+    }
+}
